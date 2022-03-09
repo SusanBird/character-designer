bdc46551008522e2df7da02be967adbecebe0e72
SusanBird <susanbird391@gmail.com>
2022-03-09 15:15:19 -0800

    update testfile

--- utils.js
+++
@@ -1,9 +0,0 @@
-
-const test = QUnit.test;
-
-test('passing test', (expect) => {
-
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(true, true);
-});
